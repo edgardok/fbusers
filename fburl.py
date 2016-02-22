0e1816bb830289bc3c43cfc5b822160b1ad578fc
edgardok <edgardo.krause@gmail.com>
2016-02-22 11:40:58 -0300

    otra prueba...

--- fburl.py
+++ fburl.py
@@ -1,25 +1,63 @@
-import urllib2
-
-def existe(url):
-    return True
-    return False
-
-
-id=0
-archivo=open("fbusers.txt","w")
+from urllib2 import *
+import datetime
+ 
+ 
+ 
+print("Fb py v0.1")
+##nombre=input("ingrese nombre para archivo de salida")
+print("---")
+archivo= open("test.txt","w")
+info=""
 cadena = ""
-url = 'https://www.facebook.com/' 
-while id<10000000000000:  
-    aux = url+str(id)
-    print aux
-    id+=1
-    try:
-        usock = urllib2.urlopen(aux)
-        DATA = usock.read() 
-        usock.close()
-    except :
-        continue
-    if (existe(DATA)):
-            cadena=cadena+aux+"\n"
-    archivo.write(cadena)
-    archivo.close
+ 
+error404=0
+suma=0
+guardados=0
+
+def existe(DATA):
+   if "no se pudo encontrar" in str(DATA).lower():
+      return false
+   else:
+      return true
+ 
+url = "http://facebook.com/"
+print("web seleccionada: "+url)
+
+ 
+ 
+Minimo=int(input("ingrese un valor minimo de ID"))
+Maximo=int(input("ingrese un valor maximo de ID"))
+ 
+contador=0
+print("hora de inicio "+str(datetime.datetime.now()))
+print("id minimo:"+str(Minimo)+" id maximo:"+str(Maximo))
+for id in range(Minimo,Maximo):
+   aux = url+str(id)
+   print aux
+   try:
+      usock = urllib2.urlopen(aux)
+      print "estamos en try"
+      DATA = usock.read()
+      if (existe(DATA)):
+            info=info+str(DATA)+"\n"
+      usock.close()               
+      print(aux)
+      print(info)
+ 
+ 
+      if contador == 5: # <---- Aca pone cada cuantos id queres guardar, yo le puse un numero bajo para probar
+         contador=0
+         guardados+=1
+         salida=str(guardados)+".txt"
+         print(salida+" salida")
+         archivo=open(salida,"w")
+         archivo.write(info)
+         archivo.close()
+         info=""
+      else:
+         contador+=1
+   except :
+      error404+=1
+      continue
+ 
+   cadena=cadena+aux+"\n"
